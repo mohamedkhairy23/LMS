061e704d400c6492ea9f83024d9bc836f29d1f4c
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-12-13 15:36:14 +0200

    initial commit

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,6 @@ import { app } from "./app";
 import connectDB from "./utils/db";
 require("dotenv").config();
 
-// create server
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
   connectDB();
