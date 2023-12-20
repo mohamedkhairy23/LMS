ff8370e9f7b811dbc98b69a5e40e0e48f53598b1
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-12-20 11:30:06 +0200

    endpoints for getting all notifications and updating notification read status

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ import { ErrorMiddleware } from "./middlewares/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
+import notificationRouter from "./routes/notification.route";
 
 app.use(express.json({ limit: "50mb" }));
 
@@ -22,6 +23,7 @@ app.use(
 app.use("/api/v1", userRouter);
 app.use("/api/v1", courseRouter);
 app.use("/api/v1", orderRouter);
+app.use("/api/v1", notificationRouter);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
---
+++ server/controllers/notification.controller.ts
@@ -0,0 +1,55 @@
+import NotificationModel from "../models/notification.model";
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncError } from "../middlewares/catchAsyncError";
+import ErrorHandler from "../utils/ErrorHandler";
+
+// @desc     Get all notifications
+// @route    POST /api/v1/
+// @access   Private("admin")
+export const getNotifications = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1,
+      });
+
+      res.status(200).json({
+        success: true,
+        notifications,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// @desc     Update notification status
+// @route    POST /api/v1/update-notification/:id
+// @access   Private("admin")
+export const updateNotification = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notification = await NotificationModel.findById(req.params.id);
+      if (!notification) {
+        return next(new ErrorHandler("Notification not found", 404));
+      } else {
+        notification.status
+          ? (notification.status = "read")
+          : notification.status;
+      }
+
+      await notification.save();
+
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1,
+      });
+
+      res.status(201).json({
+        success: true,
+        notifications,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
