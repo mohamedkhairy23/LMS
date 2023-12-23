51559a1ea28afbf179758b5122e453f37f9487a5
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-12-23 15:54:31 +0200

    Header and hero section

---
+++ client/app/components/Route/Hero.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { FC } from "react";
+import { BiSearch } from "react-icons/bi";
+
+type Props = {};
+
+const Hero: FC<Props> = (props) => {
+  return (
+    <section className="bg-white dark:bg-gray-900 h-100vh">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            Improve Your Online Learning Experience Better Instantly
+          </h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            We have 40k+ online coursesn and 4k+ online registered student. Find
+            your desired courses from them
+          </p>
+          <div className="mb-3">
+            <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+              <input
+                type="search"
+                className="relative m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary dark:focus:text-neutral-200"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon1"
+              />
+              <button
+                className="relative z-[2] flex items-center rounded-r bg-blue-700 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+                type="button"
+                id="button-addon1"
+                data-te-ripple-init=""
+                data-te-ripple-color="light"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <Image
+            className="rounded"
+            src={require("../../../public/assets/hero.jpg")}
+            alt="mockup"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
